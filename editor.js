9d2dfd47e0ae43dde0cbd80b05363f78dcbb0184
thevostle <technic.dima@gmail.com>
2021-08-21 00:35:47 +0300

    Create editor.js

---
+++ editor.js
@@ -0,0 +1,30 @@
+let generated = ""; // html-код макета
+let elements = [];
+
+element = {
+	type: "",
+	coords: [],
+	size: 0,
+	settings: [],
+};
+
+// добавляет в строку generated данные об одном элементе
+function addElementToText(element) {
+	generated = generated + "<" + element["type"] + ">";
+	// добавляем атрибуты
+	// for (i in element.settings) generated += "${i.name}=\'${i.data}\'"
+	generated = generated + "</" + element["type"] + ">";
+}
+
+// генерирует полный html-код макета
+function generateHtml() {
+	for (i of elements) addElementToText(i);
+}
+
+function movebg() {
+	document.getElementById("cell").style.backgroundSize =
+		ranger.value.toString() + "px " + ranger.value.toString() + "px";
+}
+
+var ranger = document.getElementById("changer");
+ranger.addEventListener("change", movebg, false);
